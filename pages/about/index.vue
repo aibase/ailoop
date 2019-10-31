9d1dabd7d1bbef69c6347f7c9c5bb81aaf581c37
stefian <v_ianta@yahoo.com>
2019-10-31 08:44:51 -0300

    added posts, admin and about folders

---
+++ pages/about/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about-page">
+    <h1>Dual Blog Social Network</h1>
+    <p>2 types of posts: Projects & Services.</p>
+    <p>Each post has its own data & code database.</p>
+    <p>Each post has 8 matrix structures: Social, Bank, Terms, Book, Input, Tasks, Output, Pay.</p>
+    <p>JS Scripts from each posts can be applied to data and code of any accessible post in your social network.</p>
+    <p>Services can auto-chain in solutions for Projects.</p>
+    <p>Solutions are ranked and implemented automatically on Project Terms.</p>
+    <h1>Free Market Learning for all.</h1>
+  </div>
+</template>
+
+<style scoped>
+.about-page {
+  text-align: center;
+  padding: 30px;
+}
+</style>
