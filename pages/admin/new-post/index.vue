e2c5072044d862f627d1f119bb4150259450f227
stefian <v_ianta@yahoo.com>
2019-11-01 08:15:07 -0300

    added the template and UI components for the new-post page

---
+++ pages/admin/new-post/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="admin-new-post-page">
+    <section class="new-post-form">
+      <form>
+        <AppControlInput v-model="editedPost.author">Author Name</AppControlInput>
+        <AppControlInput v-model="editedPost.title">Title</AppControlInput>
+        <AppControlInput v-model="editedPost.thumbnailLink">Thumbnail Link</AppControlInput>
+        <AppControlInput control-type="textarea" v-model="editedPost.content">Content</AppControlInput>
+        <AppButton type="submit">Save</AppButton>
+        <AppButton
+          type="button"
+          style="margin-left: 10px"
+          btn-style="cancel"
+          @click="onCancel"
+        >Cancel</AppButton>
+      </form>
+    </section>
+  </div>
+</template>
