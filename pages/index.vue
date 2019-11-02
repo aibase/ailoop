05179b29a30547f831d5ef1c4d1f4d2034cca448
stefian <v_ianta@yahoo.com>
2019-11-01 22:15:44 -0300

    added asyncData with callback to ensure page is built in the server

--- pages/index.vue
+++ pages/index.vue
@@ -14,34 +14,35 @@ export default {
   components: {
     PostList
   },
-  asyncData() {
-    return {};
-  },
-  data() {
-    return {
-      loadedPosts: []
-    };
-  },
-  created() {
+  asyncData(context, callback) {
+    // executed on server
     setTimeout(() => {
-      this.loadedPosts = [
-        {
-          id: "1",
-          title: "First Post",
-          previewText: "This is our first post!",
-          thumbnail:
-            "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
-        },
-        {
-          id: "2",
-          title: "Second Post",
-          previewText: "This is our second post!",
-          thumbnail:
-            "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
-        }
-      ];
+      callback(null, {
+        loadedPosts: [
+          {
+            id: "1",
+            title: "First Post",
+            previewText: "This is our first post!",
+            thumbnail:
+              "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
+          },
+          {
+            id: "2",
+            title: "Second Post",
+            previewText: "This is our second post!",
+            thumbnail:
+              "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
+          }
+        ]
+      });
     }, 1500);
-  }
+  },
+  // data() { // executed on client
+  //   return {
+  //     loadedPosts: []
+  //   };
+  // },
+  created() {}
 };
 </script>
 
