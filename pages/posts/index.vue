948e9567f9cd2fd2064d8724de84c9636a3ee442
stefian <v_ianta@yahoo.com>
2019-11-02 16:21:55 -0300

    added nuxtServerInit to intialize the vuex store once instead for everypage

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -11,42 +11,6 @@ export default {
   components: {
     PostList
   },
-  // same as asyncData - method added by Nuxt to run on server or client
-  fetch(context) {
-    // executed on server
-    if (context.store.state.loadedPosts > 0) {
-      return null;
-    } // to exexcute loading posts if any
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve({
-          loadedPosts: [
-            {
-              id: "1",
-              title: "First Post",
-              previewText: "This is our first post!",
-              thumbnail:
-                "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
-            },
-            {
-              id: "2",
-              title: "Second Post",
-              previewText: "This is our second post!",
-              thumbnail:
-                "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
-            }
-          ]
-        });
-      }, 1000);
-      // reject(new Error());
-    })
-      .then(data => {
-        context.store.commit("setPosts", data.loadedPosts);
-      })
-      .catch(e => {
-        context.error(e);
-      });
-  },
   computed: {
     loadedPosts() {
       return this.$store.getters.loadedPosts;
