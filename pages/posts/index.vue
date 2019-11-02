f6b6f067b5dc6d4aae20e95e56330fa2b6554340
stefian <v_ianta@yahoo.com>
2019-11-02 09:41:43 -0300

    added asyncData to posts index.vue to return dummy topPosts

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="posts-page">
-    <PostList />
+    <PostList :posts="topPosts" />
   </div>
 </template>
 
@@ -10,6 +10,29 @@ import PostList from "@/components/Posts/PostList";
 export default {
   components: {
     PostList
+  },
+  asyncData(context, callback) {
+    // executed on server
+    setTimeout(() => {
+      callback(null, {
+        topPosts: [
+          {
+            id: "3",
+            title: "Third Post",
+            previewText: "This is our third post!",
+            thumbnail:
+              "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
+          },
+          {
+            id: "4",
+            title: "Fourth Post",
+            previewText: "This is our fourth post!",
+            thumbnail:
+              "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
+          }
+        ]
+      });
+    }, 1500);
   }
 };
 </script>
