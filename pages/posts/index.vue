6de751311c0430944f6488839402608087b81dc2
stefian <v_ianta@yahoo.com>
2019-11-02 11:39:07 -0300

    Using Promises in asyncData with commented reject() function

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -14,27 +14,27 @@ export default {
   asyncData(context) {
     // executed on server
     return new Promise((resolve, reject) => {
-      // setTimeout(() => {
-      //   resolve({
-      //     loadedPosts: [
-      //       {
-      //         id: "1",
-      //         title: "First Post",
-      //         previewText: "This is our first post!",
-      //         thumbnail:
-      //           "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
-      //       },
-      //       {
-      //         id: "2",
-      //         title: "Second Post",
-      //         previewText: "This is our second post!",
-      //         thumbnail:
-      //           "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
-      //       }
-      //     ]
-      //   });
-      // }, 1000);
-      reject(new Error());
+      setTimeout(() => {
+        resolve({
+          loadedPosts: [
+            {
+              id: "1",
+              title: "First Post",
+              previewText: "This is our first post!",
+              thumbnail:
+                "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
+            },
+            {
+              id: "2",
+              title: "Second Post",
+              previewText: "This is our second post!",
+              thumbnail:
+                "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
+            }
+          ]
+        });
+      }, 1000);
+      // reject(new Error());
     })
       .then(data => {
         return data;
