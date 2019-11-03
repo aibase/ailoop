e443cdbd0382e92c350725f34b1912b187fef1be
stefian <v_ianta@yahoo.com>
2019-11-03 12:26:41 -0400

    adding editing/updating post in FB RTDB

--- pages/admin/_postId/index.vue
+++ pages/admin/_postId/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="admin-post-page">
     <section class="update-form">
-      <AdminPostForm :post="loadedPost" />
+      <AdminPostForm :post="loadedPost" @submit="onSubmitted" />
     </section>
   </div>
 </template>
@@ -26,6 +26,21 @@ export default {
         };
       })
       .catch(e => context.error(e));
+  },
+  methods: {
+    onSubmitted(editedPost) {
+      axios
+        .put(
+          "https://ailoop.firebaseio.com/posts/" +
+            this.$route.params.postId +
+            ".json",
+          editedPost
+        )
+        .then(res => {
+          this.$router.push("/admin");
+        })
+        .catch(e => console.log(e));
+    }
   }
 };
 </script>
--- pages/admin/new-post/index.vue
+++ pages/admin/new-post/index.vue
@@ -22,7 +22,9 @@ export default {
           ...postData,
           updatedDate: new Date()
         })
-        .then(result => console.log(result))
+        .then(result => {
+          this.$router.push("/admin");
+        })
         .catch(e => console.log(e));
     }
   }
