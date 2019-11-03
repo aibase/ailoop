03c03572571822a64ef59ddcdb415a7363829ad3
stefian <v_ianta@yahoo.com>
2019-11-03 11:56:34 -0400

    linked the admin editing for existing posts to FB RTDB

--- pages/admin/_postId/index.vue
+++ pages/admin/_postId/index.vue
@@ -7,6 +7,7 @@
 </template>
 
 <script>
+import axios from "axios";
 import AdminPostForm from "@/components/Admin/AdminPostForm";
 
 export default {
@@ -14,16 +15,17 @@ export default {
   components: {
     AdminPostForm
   },
-  data() {
-    return {
-      loadedPost: {
-        author: "Stefian",
-        title: "My meta Post",
-        content: "Super meta, thanks to Evo!",
-        thumbnailLink:
-          "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
-      }
-    };
+  asyncData(context) {
+    return axios
+      .get(
+        "https://ailoop.firebaseio.com/posts/" + context.params.postId + ".json"
+      )
+      .then(res => {
+        return {
+          loadedPost: res.data
+        };
+      })
+      .catch(e => context.error(e));
   }
 };
 </script>
