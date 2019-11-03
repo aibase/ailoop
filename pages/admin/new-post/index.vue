aed035d39b5e8d8dd33c87a37f1268160c51f6d8
stefian <v_ianta@yahoo.com>
2019-11-03 09:46:32 -0400

    added storing post in Firebase

--- pages/admin/new-post/index.vue
+++ pages/admin/new-post/index.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="admin-new-post-page">
     <section class="new-post-form">
-      <AdminPostForm />
+      <AdminPostForm @submit="onSubmitted" />
     </section>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 import AdminPostForm from "@/components/Admin/AdminPostForm";
 
 export default {
   layout: "admin",
   components: {
     AdminPostForm
+  },
+  methods: {
+    onSubmitted(postData) {
+      axios
+        .post("https://ailoop.firebaseio.com/posts.json", postData)
+        .then(result => console.log(result))
+        .catch(e => console.log(e));
+    }
   }
 };
 </script>
