622a04ef17ac2d51729544e7450c4c909c1be4a1
stefian <v_ianta@yahoo.com>
2019-11-05 20:45:17 -0400

    added 3 core componets as plugins: AppButton, AppControlInput, PostList

--- components/Admin/AdminPostForm.vue
+++ components/Admin/AdminPostForm.vue
@@ -11,14 +11,7 @@
 </template>
 
 <script>
-import AppControlInput from "@/components/UI/AppControlInput";
-import AppButton from "@/components/UI/AppButton";
-
 export default {
-  components: {
-    AppControlInput,
-    AppButton
-  },
   props: {
     post: {
       type: Object,
--- pages/admin/auth/index.vue
+++ pages/admin/auth/index.vue
@@ -17,17 +17,10 @@
 </template>
 
 <script>
-import AppControlInput from "@/components/UI/AppControlInput";
-import AppButton from "@/components/UI/AppButton";
-
 export default {
   layout: "admin",
   name: "AdminAuthPage",
   layout: "admin",
-  components: {
-    AppControlInput,
-    AppButton
-  },
   data() {
     return {
       isLogin: true
