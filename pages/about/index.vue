ef397dcabefdcc532cfc1671ef5221786b4fb07d
stefian <v_ianta@yahoo.com>
2019-11-07 15:52:07 -0400

    added firebase project

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="about-page">
-    <h1>Dual Blog Social Network</h1>
+    <h1>ai social network</h1>
     <p>2 types of posts: Projects & Services.</p>
-    <p>Each post has its own data & code database.</p>
-    <p>Each post has 8 matrix structures: Social, Bank, Terms, Book, Input, Tasks, Output, Pay.</p>
-    <p>JS Scripts from each posts can be applied to data and code of any accessible post in your social network.</p>
+    <p>Each post has its own data & code spreadsheet.</p>
+    <p>Each post has 8 tabs: Social, Bank, Terms, Deal, Input, Tasks, Output, Pay.</p>
+    <p>JS Scripts from each posts can be applied to data of any accessible post in your social network.</p>
     <p>Services can auto-chain in solutions for Projects.</p>
     <p>Solutions are ranked and implemented automatically on Project Terms.</p>
-    <h1>Free Market Learning for all.</h1>
+    <h1>decentralized social network</h1>
   </div>
 </template>
 
