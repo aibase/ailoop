66384d1c28560f13d5488410cfa1866fc89fd040
stefian <v_ianta@yahoo.com>
2019-11-07 18:38:55 -0400

    added heade and sidenav for admin pages

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -1,8 +1,28 @@
 <template>
   <div>
+    <TheHeader @sidenavToggle="displaySidenav = !displaySidenav" />
+    <TheSidenav :show="displaySidenav" @close="displaySidenav = false" />
     <nuxt />
   </div>
 </template>
+
+<script>
+import TheHeader from "@/components/Navigation/TheHeader";
+import TheSidenav from "@/components/Navigation/TheSidenav";
+
+export default {
+  // middleware: "log",
+  components: {
+    TheHeader,
+    TheSidenav
+  },
+  data() {
+    return {
+      displaySidenav: false
+    };
+  }
+};
+</script>
 
 <style>
 html {
