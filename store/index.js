7ecf6b8379c93083eaaeddbc18c3c5a4e76eaf9a
stefian <v_ianta@yahoo.com>
2019-11-08 19:16:40 -0400

    Dummy axios Post request from the client to an API Middleware on the server

--- store/index.js
+++ store/index.js
@@ -88,6 +88,8 @@ const createStore = () => {
             localStorage.setItem('tokenExpiration', new Date().getTime() + Number.parseInt(result.expiresIn) * 1000);
             Cookie.set('jwt', result.idToken);
             Cookie.set('expirationDate', new Date().getTime() + Number.parseInt(result.expiresIn) * 1000);
+            // Dummy axios Post request to an API Middleware
+            return this.$axios.$post('http://localhost:3000/api/track-data', { data: 'Authenticated!' })
           })
           .catch(e => console.log(e));
       },
