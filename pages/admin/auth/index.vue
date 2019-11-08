e40f165efc24ac440bb29a8d38bcbd9c102a1e77
stefian <v_ianta@yahoo.com>
2019-11-07 20:23:48 -0400

    removed pages/admin/auth/ from git ignore

---
+++ pages/admin/auth/index.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="admin-auth-page">
+    <div class="auth-container">
+      <form @submit.prevent="onSubmit">
+        <AppControlInput type="email" v-model="email">E-Mail Address</AppControlInput>
+        <AppControlInput type="password" v-model="password">Password</AppControlInput>
+        <AppButton type="submit">{{ isLogin ? 'Login' : 'Sign Up' }}</AppButton>
+        <AppButton
+          type="button"
+          btn-style="inverted"
+          style="margin-left: 10px"
+          @click="isLogin = !isLogin"
+        >Switch to {{ isLogin ? 'Signup' : 'Login' }}</AppButton>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: "admin",
+  name: "AdminAuthPage",
+  layout: "admin",
+  data() {
+    return {
+      isLogin: true,
+      email: "",
+      password: ""
+    };
+  },
+  methods: {
+    onSubmit() {
+      let authUrl =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=" +
+        process.env.fbAPIKey;
+      if (!this.isLogin) {
+        authUrl =
+          "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" +
+          process.env.fbAPIKey;
+      }
+      this.$axios
+        .$post(authUrl, {
+          email: this.email,
+          password: this.password,
+          returnSecureToken: true
+        })
+        .then(result => {
+          console.log(result);
+        })
+        .catch(e => console.log(e));
+    }
+  }
+};
+</script>
+
+<style scoped>
+.admin-auth-page {
+  padding: 20px;
+}
+
+.auth-container {
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  box-shadow: 0 2px 2px #ccc;
+  width: 300px;
+  margin: auto;
+  padding: 10px;
+  box-sizing: border-box;
+}
+</style>
