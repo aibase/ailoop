80c10f055c705248892f7c807a3287d9686b115a
stefian <v_ianta@yahoo.com>
2019-11-12 15:30:38 -0400

    changed previeText from textarea to default input

--- components/Admin/AdminPostForm.vue
+++ components/Admin/AdminPostForm.vue
@@ -2,9 +2,9 @@
   <form @submit.prevent="onSave">
     <AppControlInput v-model="editedPost.creator">Author Name</AppControlInput>
     <AppControlInput v-model="editedPost.title">Title</AppControlInput>
+    <AppControlInput v-model="editedPost.previewText">Preview Text</AppControlInput>
     <AppControlInput v-model="editedPost.imageUrl">Thumbnail Link</AppControlInput>
     <AppControlInput control-type="textarea" v-model="editedPost.content">Content</AppControlInput>
-    <AppControlInput control-type="textarea" v-model="editedPost.previewText">Preview Text</AppControlInput>
     <AppButton type="submit">Save</AppButton>
     <AppButton type="button" style="margin-left: 10px" btn-style="cancel" @click="onCancel">Cancel</AppButton>
   </form>
